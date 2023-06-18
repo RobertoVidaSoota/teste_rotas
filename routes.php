5e5b8e8a86778869b848ff759bb40e0206f8ac25
RobertoVida <roberto50005000@gmail.com>
2023-06-17 22:10:21 -0300

    Sat, Jun 17, 2023 10:10:21 PM

--- routes.php
+++ routes.php
@@ -3,23 +3,15 @@
 // VERSÃO DEPLOY VERCEL
 
 if(isset($_SERVER['REQUEST_URI'])) // verificar inicialização caminho do arquivo
-{
-    $path = explode("/", $_SERVER['REQUEST_URI']);
-    if($path[1]) // verificar se o nome do arquivo esta presente na url
+{    
+    // verificar se o arquivo existe
+    if(@is_file("/var/task/user". $_SERVER['REQUEST_URI'].".php")) 
     {
-        echo $_SERVER['REQUEST_URI'];
-        // if(@is_file("/var/task/user/".explode("/", $_SERVER['REQUEST_URI']).".php")) // verificar se o arquivo existe
-        // {
-        //     require_once explode("/", $_SERVER['REQUEST_URI']).".php";
-        //     exit;
-        // }
-        // require_once "404.php";
-        // exit;
+        require_once  $_SERVER['REQUEST_URI'].".php";
+        exit;
     }
-    else
-    {
-        require_once "inicio.php";
-    }
+    require_once "404.php";
+    exit;
 }
 else
 {
