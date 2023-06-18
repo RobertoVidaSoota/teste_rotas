025342cb64be485c31837007108aa163730181f3
RobertoVida <roberto50005000@gmail.com>
2023-06-17 22:47:10 -0300

    Sat, Jun 17, 2023 10:47:10 PM

--- routes.php
+++ routes.php
@@ -21,8 +21,8 @@ if(isset($_SERVER['REQUEST_URI'])) // verificar inicialização caminho do arqui
         exit;
     }
     // url estando vazia sera carregada o inicio.php
-    echo "<h2> url: $novaUrl</h2>";
-    if($novaUrl === "")
+    echo "<h2> url: $urlAntiga</h2>";
+    if($urlAntiga === "")
     {
         require_once "inicio.php";
         exit;
